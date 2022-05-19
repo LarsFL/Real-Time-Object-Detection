c1def01538f030a733c4875be241460de38975f2
Lars Looijenga <lars.looijenga@gmail.com>
2022-05-19 23:31:05 +0200

    Added a manual mode

--- app_threaded2.py
+++ app_threaded2.py
@@ -17,6 +17,8 @@ PATH_TO_LABELS = os.path.join(CWD_PATH,'object_detection', 'data', 'mscoco_label
 NUM_CLASSES = 90
 IMAGE_WIDTH = 1920
 IMAGE_HEIGHT = 1080
+targetX, targetY = 500, 500
+targetDim = 250
 
 class OutputFrame:
     def __init__(self):
@@ -70,17 +72,28 @@ def predict(threadName):
           feed_dict={image_tensor: image_np_expanded})
 
 
+def click_callback(event, x, y, flags, param):
+    global targetX, targetY
+    if event == 1:
+        targetX, targetY = x, y
+
+def change_zoom(x):
+    global targetDim
+    zoom = cv2.getTrackbarPos('Zoom','Original')
+    zoom *= 50
+    targetDim = zoom + 200
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', type=int, default=0, help='The video source')
     parser.add_argument('--crop', action='store_true', help='Crop video to cats')
+    parser.add_argument('--manual', action='store_true', help='Move the crop manually by clicking')
     opt = parser.parse_args()
 
-    source, crop = opt.source, opt.crop
+    source, crop, manual = opt.source, opt.crop, opt.manual
     currentCrop = [0, 1000, 0, 1000]
     centerX, centerY = 500, 500
-    targetX, targetY = 500, 500
-    largestDim, targetDim = 250, 250
+    largestDim = 250
     lastTargetId = 0
     lostTargetCount = 0
     catBox = None
@@ -107,58 +120,65 @@ if __name__ == "__main__":
     output_frame = OutputFrame()
 
     webcam_thread = WebcamThread("Webcam Thread")
-    predictor_thread = PredictorThread("Predictor Thread")
     webcam_thread.start()
-    predictor_thread.start()
+    if not manual:
+        predictor_thread = PredictorThread("Predictor Thread")
+        predictor_thread.start()
 
     print("---- Starting CatTracker ----")
+
+    cv2.namedWindow('Original')
+    if manual:
+        cv2.setMouseCallback('Original', click_callback)
+        cv2.createTrackbar('Zoom','Original',1,10,change_zoom)
 
     while True:
         if output_frame.boxes == ():
             to_show = output_frame.frame
         else:
             to_show = output_frame.frame
-            catBoxes = vis_util.visualize_boxes_and_labels_on_image_array(
-              to_show,
-              np.squeeze(output_frame.boxes[0]),
-              np.squeeze(output_frame.boxes[2]).astype(np.int32),
-              np.squeeze(output_frame.boxes[1]),
-              category_index,
-              use_normalized_coordinates=True,
-              line_thickness=8,
-              width=IMAGE_WIDTH,
-              height=IMAGE_HEIGHT,
-              crop=crop)
+        if not manual and not output_frame.boxes == ():
+                catBoxes = vis_util.visualize_boxes_and_labels_on_image_array(
+                to_show,
+                np.squeeze(output_frame.boxes[0]),
+                np.squeeze(output_frame.boxes[2]).astype(np.int32),
+                np.squeeze(output_frame.boxes[1]),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=8,
+                width=IMAGE_WIDTH,
+                height=IMAGE_HEIGHT,
+                crop=crop)
 
-            firstTargetX, firstTargetY, firstTargetDim, secondTargetX, secondTargetY, secondTargetDim = 0,0,0,0,0,0
-            if catBoxes and len(catBoxes) != 0:
-                firstTargetX = int((catBoxes[0][1] + catBoxes[0][3]) / 2)
-                firstTargetY = int((catBoxes[0][0] + catBoxes[0][2]) / 2)
-                firstTargetDim = int(max([int(catBoxes[0][3]) - int(catBoxes[0][1]), int(catBoxes[0][2]) - int(catBoxes[0][0])]) * 1.3)
-                if len(catBoxes) > 1:
-                    secondTargetX = int((catBoxes[1][1] + catBoxes[1][3]) / 2)
-                    secondTargetY = int((catBoxes[1][0] + catBoxes[1][2]) / 2)
-                    secondTargetDim = int(max([int(catBoxes[1][3]) - int(catBoxes[1][1]), int(catBoxes[1][2]) - int(catBoxes[1][0])]) * 1.3)
+                firstTargetX, firstTargetY, firstTargetDim, secondTargetX, secondTargetY, secondTargetDim = 0,0,0,0,0,0
+                if catBoxes and len(catBoxes) != 0:
+                    firstTargetX = int((catBoxes[0][1] + catBoxes[0][3]) / 2)
+                    firstTargetY = int((catBoxes[0][0] + catBoxes[0][2]) / 2)
+                    firstTargetDim = int(max([int(catBoxes[0][3]) - int(catBoxes[0][1]), int(catBoxes[0][2]) - int(catBoxes[0][0])]) * 1.3)
+                    if len(catBoxes) > 1:
+                        secondTargetX = int((catBoxes[1][1] + catBoxes[1][3]) / 2)
+                        secondTargetY = int((catBoxes[1][0] + catBoxes[1][2]) / 2)
+                        secondTargetDim = int(max([int(catBoxes[1][3]) - int(catBoxes[1][1]), int(catBoxes[1][2]) - int(catBoxes[1][0])]) * 1.3)
 
-            if (secondTargetX == 0 and lostTargetCount == 0 and firstTargetX != 0):
-                targetX = firstTargetX
-                targetY = firstTargetY
-                targetDim = firstTargetDim
-
-            if (secondTargetX != 0):
-                if (abs(secondTargetX - firstTargetX) < (.3 * IMAGE_WIDTH) and abs(secondTargetY - firstTargetY) < (.3 * IMAGE_HEIGHT)):
-                    lostTargetCount = 2
-                    targetX = int((firstTargetX + secondTargetX) / 2)
-                    targetY = int((firstTargetY + secondTargetY) / 2)
-                    targetDim = int(max(firstTargetDim, secondTargetDim) * 1.85)
-                else:
+                if (secondTargetX == 0 and lostTargetCount == 0 and firstTargetX != 0):
                     targetX = firstTargetX
                     targetY = firstTargetY
                     targetDim = firstTargetDim
-                    lostTargetCount = lostTargetCount - 1 if lostTargetCount != 0 else 0
-            
-            if (secondTargetX == 0 and lostTargetCount > 0):
-                lostTargetCount -= 1
+
+                if (secondTargetX != 0):
+                    if (abs(secondTargetX - firstTargetX) < (.3 * IMAGE_WIDTH) and abs(secondTargetY - firstTargetY) < (.3 * IMAGE_HEIGHT)):
+                        lostTargetCount = 2
+                        targetX = int((firstTargetX + secondTargetX) / 2)
+                        targetY = int((firstTargetY + secondTargetY) / 2)
+                        targetDim = int(max(firstTargetDim, secondTargetDim) * 1.85)
+                    else:
+                        targetX = firstTargetX
+                        targetY = firstTargetY
+                        targetDim = firstTargetDim
+                        lostTargetCount = lostTargetCount - 1 if lostTargetCount != 0 else 0
+                
+                if (secondTargetX == 0 and lostTargetCount > 0):
+                    lostTargetCount -= 1
 
         newCenterX = int(centerX * .98 + targetX * .02)
         if newCenterX - centerX > 10:
@@ -176,7 +196,6 @@ if __name__ == "__main__":
         elif largestDim - newLargestDim > 10:
             newLargestDim = largestDim - 10
         centerX, centerY, largestDim = newCenterX, newCenterY, newLargestDim
-
 
         startY = int(centerY - largestDim / 2)
         endY = int(centerY + largestDim / 2)
@@ -201,11 +220,22 @@ if __name__ == "__main__":
         currentCrop = [int(max(0, startY)), int(min(IMAGE_HEIGHT, endY)), int(max(0, startX)), int(min(IMAGE_WIDTH, endX))]
         imgTest = to_show[currentCrop[0]: currentCrop[1], currentCrop[2]: currentCrop[3]]
         resizedImgTest = cv2.resize(imgTest, (500, 500), interpolation=cv2.INTER_AREA)
-        
+        if manual:
+            vis_util.draw_bounding_box_on_image_array(
+            to_show,
+            currentCrop[0],
+            currentCrop[2],
+            currentCrop[1],
+            currentCrop[3],
+            use_normalized_coordinates=False,
+            color='purple'
+            )
+
         if crop:
-            cv2.imshow('frame', resizedImgTest)
+            cv2.imshow('Cat', resizedImgTest)
+            cv2.imshow('Original', to_show)
         else:
-            cv2.imshow('frame', to_show)
+            cv2.imshow('Original', to_show)
             
         if cv2.waitKey(1) & 0xFF == ord('q'):
             done = True
